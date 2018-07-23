ef0af93872cbdc8680de24ca14147042492a1067
fiolam <fiola.mulliqi@wework.com>
2018-07-23 22:51:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your solution in this file!
 let driver={}
 function updateDriverWithKeyAndValue(driver,key,value){
+let copyOfDriver = {...driver}
+copyOfDriver[key] = value
+return copyOfDriver
 }
