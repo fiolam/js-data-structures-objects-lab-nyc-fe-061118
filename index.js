6238493f87e8350f971e0d591c3196d76f360049
fiolam <fiola.mulliqi@wework.com>
2018-07-23 22:58:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateDriverWithKeyAndValue(driver,key,value){
 return {...driver, [key]:value}
 
 }
-function updateDriverWithKeyAndValue(driver,key,value){
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
 
 return {...driver, [key]:value}
 
